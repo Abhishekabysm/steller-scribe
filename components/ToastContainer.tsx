32c598cab984aca4108223cb7add6d4d504a71e2
Abhishekabysm <abhishekanish09@gmail.com>
2025-07-10 12:44:33 +0530

    fix: adjust positioning of ToastContainer for improved alignment on mobile

--- components/ToastContainer.tsx
+++ components/ToastContainer.tsx
@@ -9,7 +9,7 @@ interface ToastContainerProps {
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ toasts, removeToast }) => {
   return (
-    <div className="fixed top-20 right-2 sm:right-4 z-50 w-full max-w-sm sm:max-w-sm px-2 sm:px-0 space-y-3">
+    <div className="fixed top-20 left-1/2 -translate-x-1/2 sm:left-auto sm:right-4 sm:translate-x-0 z-50 w-full max-w-sm px-4 sm:px-0 space-y-3">
       {toasts.map((toast) => (
         <Toast key={toast.id} toast={toast} removeToast={removeToast} />
       ))}
